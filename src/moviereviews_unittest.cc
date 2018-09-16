c77212ff6840ea7fe6852591a499775edd041b02
profknight <ahknight@pipeline.sbcc.edu>
2018-09-16 12:46:24 -0700

    Updated project to not use classes and to EX04

--- src/moviereviews_unittest.cc
+++ src/moviereviews_unittest.cc
@@ -42,20 +42,23 @@
 #include <chrono>
 #include <functional>
 
+#include "gtest/gtest.h"
+
 #include "moviereviews.h"
 #include "GTestNoDeath.h"
 
-#include "gtest/gtest.h"
 namespace {
 
-using edu::sbcc::cs140::MovieReviews;
+//using edu::sbcc::cs140::predictEmptyReviews;
+//using edu::sbcc::cs140::NUMBER_MOVIES;
+//using edu::sbcc::cs140::MAX_REVIEWS;
 
 void readMovieReviews(const std::string &filename, uint8_t reviews[1000][6]) {
-    std::ifstream in("../../reviews.txt");
+    std::ifstream in(filename);
 
     size_t count = 0;
     for (; !in.eof(); count++) {
-        for (size_t j = 0; j < MovieReviews::NUMBER_MOVIES; j++) {
+        for (size_t j = 0; j < NUMBER_MOVIES; j++) {
             uint32_t value;
             in >> value;
             reviews[count][j] = static_cast<uint8_t >(value);
@@ -100,14 +103,20 @@ protected:
 
 uint MovieReviewsTest::_testScore = 0;
 
+uint8_t DEFAULT_REVIEWS[][NUMBER_MOVIES] = {
+        {3, 1, 5, 2, 1, 5},
+        {4, 2, 1, 4, 2, 4},
+        {3, 1, 2, 4, 4, 1},
+        {5, 1, 4, 2, 4, 2}
+};
+
 TEST_F(MovieReviewsTest, DefaultCase) {
     // This test is named "Identity", it checks that the basic identity
     // complex number, i, has the correct values for real and imaginary
     // parts.
 
-    MovieReviews reviews1;
     uint8_t userReview1[] = {0, 1, 0, 2, 0, 3};
-    reviews1.predictEmptyReviews(userReview1);
+    predictEmptyReviews(DEFAULT_REVIEWS, userReview1, 4);
 
     ASSERT_EQ(5, userReview1[0]);
     ASSERT_EQ(1, userReview1[1]);
@@ -117,7 +126,7 @@ TEST_F(MovieReviewsTest, DefaultCase) {
     ASSERT_EQ(3, userReview1[5]);
 
     uint8_t userReview2[] = {5, 0, 4, 0, 3, 0};
-    reviews1.predictEmptyReviews(userReview2);
+    predictEmptyReviews(DEFAULT_REVIEWS, userReview2, 4);
 
     ASSERT_EQ(5, userReview2[0]);
     ASSERT_EQ(1, userReview2[1]);
@@ -130,12 +139,11 @@ TEST_F(MovieReviewsTest, DefaultCase) {
 }
 
 TEST_F(MovieReviewsTest, ThousandReviews) {
-    uint8_t reviews[1000][6];
-    readMovieReviews("../../reviews.txt", reviews);
+    uint8_t reviews[MAX_REVIEWS][NUMBER_MOVIES];
+    readMovieReviews("../reviews.txt", reviews);
 
-    MovieReviews reviews1(reviews, 1000);
     uint8_t userReview1[] = {0, 1, 0, 2, 0, 3};
-    reviews1.predictEmptyReviews(userReview1);
+    predictEmptyReviews(reviews, userReview1, MAX_REVIEWS);
 
     ASSERT_EQ(4, userReview1[0]);
     ASSERT_EQ(1, userReview1[1]);
@@ -145,7 +153,7 @@ TEST_F(MovieReviewsTest, ThousandReviews) {
     ASSERT_EQ(3, userReview1[5]);
 
     uint8_t userReview2[] = {5, 0, 4, 0, 3, 0};
-    reviews1.predictEmptyReviews(userReview2);
+    predictEmptyReviews(reviews, userReview2, MAX_REVIEWS);
 
     ASSERT_EQ(5, userReview2[0]);
     ASSERT_EQ(3, userReview2[1]);
@@ -159,13 +167,13 @@ TEST_F(MovieReviewsTest, ThousandReviews) {
 
 TEST_F(MovieReviewsTest, ErrorCheckingSizeTooBig) {
     uint8_t reviews[1000][6];
-    readMovieReviews("../../reviews.txt", reviews);
+    readMovieReviews("../reviews.txt", reviews);
 
-    ASSERT_NO_DEATH(MovieReviews movies1(reviews, 10001), "");
     uint8_t userReview1[] = {0, 1, 0, 2, 0, 3};
-    MovieReviews movies1(reviews, 10001);
-    movies1.predictEmptyReviews(userReview1);
 
+    ASSERT_NO_DEATH(predictEmptyReviews(reviews, userReview1, 10001), "");
+
+    predictEmptyReviews(reviews, userReview1, 10001);
     ASSERT_EQ(4, userReview1[0]);
     ASSERT_EQ(1, userReview1[1]);
     ASSERT_EQ(4, userReview1[2]);
@@ -177,12 +185,11 @@ TEST_F(MovieReviewsTest, ErrorCheckingSizeTooBig) {
 }
 
 TEST_F(MovieReviewsTest, ErrorCheckingNullReviews) {
-    ASSERT_NO_DEATH(MovieReviews movies2(NULL, 1000), "");
-    MovieReviews movies2(NULL, 1000);
     uint8_t userReview2[] = {0, 1, 0, 2, 0, 3};
 
-    movies2.predictEmptyReviews(userReview2);
+    ASSERT_NO_DEATH(predictEmptyReviews(nullptr, userReview2, 1000), "");
 
+    predictEmptyReviews(nullptr, userReview2, 1000);
     ASSERT_EQ(0, userReview2[0]);
     ASSERT_EQ(1, userReview2[1]);
     ASSERT_EQ(0, userReview2[2]);
@@ -194,8 +201,7 @@ TEST_F(MovieReviewsTest, ErrorCheckingNullReviews) {
 }
 
 TEST_F(MovieReviewsTest, ErrorCheckingNullUserReview) {
-    MovieReviews movies;
-    ASSERT_NO_DEATH(movies.predictEmptyReviews(NULL), ".*");
+    ASSERT_NO_DEATH(predictEmptyReviews(DEFAULT_REVIEWS, NULL, 4), "");
     _testScore += 1;
 }
 
